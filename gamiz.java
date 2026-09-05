a04731c3616e9ee37655460066124d5d351d1fd0
jerichogamis8-bit <jerichogamis8@gmail.com>
2026-09-05 22:38:37 +0800

    ACTIVITY 2.6

---
+++ gamiz.java
@@ -0,0 +1,8 @@
+public class DemoVariables4 {
+    public static void main(String[] args) {
+
+        char myGrade = 'B', myFriendsGrade = 'C';
+
+        System.out.println("Our grade are " + myGrade + " and " + myFriendsGrade);
+    }
+}
