4a2da8bc079dc135405bde4e0ab785416f9b7738
jerichogamis8-bit <jerichogamis8@gmail.com>
2026-09-05 22:17:04 +0800

    ACTIVITY 2.2

---
+++ IT111.java
@@ -0,0 +1,20 @@
+// Filename: HelloWorld2.java
+// Written by: Jericho F. Gamis
+// Course: BSIT 1-2
+// Written on: September 2, 2026
+
+public class HelloWorld2 {
+
+    /*
+     * This program demonstrates the use of
+     * comments, print() and println().
+     */
+
+    public static void main(String[] args) {
+
+        // This prints a message on the screen
+        System.out.println("Hello, World!");
+        System.out.println("I am a new student.");
+        System.out.println("I'm ready for Java programming!");
+    }
+}
