0a9cde3832bb7241d15a2ec9f64df062facb390a
jerichogamis8-bit <jerichogamis8@gmail.com>
2026-09-05 22:35:03 +0800

    ACTIVITY 2.5

---
+++ Gamis.java
@@ -0,0 +1,12 @@
+public class DemoVariables3 {
+    public static void main(String[] args) {
+
+        double doubNum1 = 2.3, doubNum2 = 14.8, doubResult;
+
+        doubResult = doubNum1 + doubNum2;
+        System.out.println("The sum of the doubles is " + doubResult);
+
+        doubResult = doubNum1 * doubNum2;
+        System.out.println("The product of the doubles is " + doubResult);
+    }
+}
