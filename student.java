a95f0feb8fe1a79567b937b562784c66d43b02c5
jerichogamis8-bit <jerichogamis8@gmail.com>
2026-09-05 22:18:56 +0800

    ACTIVITY 2.3

---
+++ student.java
@@ -0,0 +1,9 @@
+public class DemoVariables {
+    public static void main(String[] args) {
+
+        int intNo = 296;
+
+        System.out.print("The int is ");
+        System.out.println(intNo);
+    }
+}
