b9e478a42be9eb8262c4265584f4dea8f4756f92
jerichogamis8-bit <jerichogamis8@gmail.com>
2026-09-07 00:23:47 +0800

    Create 2.17.java

---
+++ 2.17.java
@@ -0,0 +1,15 @@
+import javax.swing.JOptionPane;
+
+public class NameInput {
+
+    public static void main(String[] args) {
+
+        String name = "";
+
+        name = JOptionPane.showInputDialog("Please enter your name");
+
+        String msg = "Hello " + name + "!";
+
+        JOptionPane.showMessageDialog(null, msg);
+    }
+}
