7fb583df127245e3b618daa484fa6c2ed46369f1
jerichogamis8-bit <jerichogamis8@gmail.com>
2026-09-06 23:42:02 +0800

    ACTIVITY 2.14

---
+++ 2.14.java
@@ -0,0 +1,12 @@
+public class DemoVariables9 {
+    public static void main(String[] args) {
+
+        char myGrade = 'A';
+        char myFriendsGrade = 'C';
+
+        System.out.println("Our grade are " + myGrade + " and " + myFriendsGrade);
+
+        System.out.println("\nThis is on one line\nThis is on another");
+        System.out.println("This shows\t how\t tabs\t work");
+    }
+}
