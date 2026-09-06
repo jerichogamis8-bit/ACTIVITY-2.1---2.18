8b8c5d4074256806135f935279857968af58552c
jerichogamis8-bit <jerichogamis8@gmail.com>
2026-09-06 23:07:12 +0800

    ACTIVITY 2.12

---
+++ 2.12.java
@@ -0,0 +1,12 @@
+package helloworld.variables;
+
+public class NumericConversion {
+    public static void main(String[] args) {
+
+        int hoursWorked = 32;
+        double payRate = 5.75;
+        double grossPay = hoursWorked * payRate;
+
+        System.out.println("Gross pay is " + grossPay);
+    }
+}
