1609551de787cf23a43b0801eec7bac423d58b48
jerichogamis8-bit <jerichogamis8@gmail.com>
2026-09-06 22:42:02 +0800

    ACTIVITY 2.11

---
+++ 2.11.java
@@ -0,0 +1,17 @@
+package helloworld.variables;
+
+public class DemoVariables8 {
+    public static void main(String[] args) {
+
+        int num1 = 5, num2 = 7, num3, num4;
+
+        num3 = ++num2;
+        num4 = num1++;
+        num3++;
+
+        System.out.println("The value of num1 is " + num1);
+        System.out.println("The value of num2 is " + num2);
+        System.out.println("The value of num3 is " + num3);
+        System.out.println("The value of num4 is " + num4);
+    }
+          }
