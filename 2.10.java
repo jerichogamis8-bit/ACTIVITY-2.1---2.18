5fafe2ecce3070d64d6b1cab8d499227532c3dba
jerichogamis8-bit <jerichogamis8@gmail.com>
2026-09-06 22:39:03 +0800

    ACTIVITY 2.10

---
+++ 2.10.java
@@ -0,0 +1,10 @@
+package helloworld.variables;
+
+public class DemoVariables7 {
+    public static void main(String[] args) {
+
+        int polynomial = (3 * 4 * 4) + (5 * 4) + 6;
+
+        System.out.println("The value of the second-degree polynomial is " + polynomial);
+    }
+}
